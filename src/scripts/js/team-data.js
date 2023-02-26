68eb66edef6b3a31b72333e45dda36a816ae7700
st-coding-gh <st-coding@yandex.ru>
2023-02-26 21:38:46 +0300

    optimize images

--- src/scripts/js/team-data.js
+++ src/scripts/js/team-data.js
@@ -3,37 +3,37 @@ export function getTeamData () {
   const teamData = [
     {
       'name': 'Whisker',
-      'url': './src/images/cat-1.png',
+      'url': './src/images/cat-1.webp',
       'text': 'Quick reflexes and sharp claws make Whisker a top-notch hunter and an excellent navigator. Whisker finds humans fascinating and is always eager to learn more about them and their world'
     },
     {
       'name': 'Luna',
-      'url': './src/images/cat-2.png',
+      'url': './src/images/cat-2.webp',
       'text': 'Luna\'s keen senses and sharp eyesight make her an excellent scout and tracker, while her graceful aerial maneuvers make her a crowd-pleaser during performances. Luna\'s best accomplishment was performing a breathtaking aerial display during a major sporting event, earning thunderous applause from the crowd'
     },
     {
       'name': 'Simba',
-      'url': './src/images/cat-3.png',
+      'url': './src/images/cat-3.webp',
       'text': 'Simba is a powerful and agile feline, capable of quick turns and daring acrobatic feats in the air. His loud roar can intimidate and scare away unwanted intruders. Simba dreams of becoming a famous flying cat athlete and setting new records in the air, as well as making new friends and experiencing new adventures'
     },
     {
       'name': 'Blaze',
-      'url': './src/images/cat-4.png',
+      'url': './src/images/cat-4.webp',
       'text': 'Blaze always had a sense of adventure and loved to explore. Joining the team was the perfect way for him to put his skills to use and see the world. Blaze thinks humans can be helpful and kind, but also a bit boring.'
     },
     {
       'name': 'Whirlwind',
-      'url': './src/images/cat-5.png',
+      'url': './src/images/cat-5.webp',
       'text': 'Excellent agility and speed, can fly through tight spaces with ease. Whirlwind has a deep respect for humans, and admires their ability to innovate and create new technologies.'
     },
     {
       'name': 'Thunderpaws',
-      'url': './src/images/cat-6.png',
+      'url': './src/images/cat-6.webp',
       'text': 'xceptional strength and endurance, can carry heavy loads long distances. Thunderpaws once flew a critically injured animal to a veterinarian over 100 miles away, saving its life'
     },
     {
       'name': 'Starlight',
-      'url': './src/images/cat-7.png',
+      'url': './src/images/cat-7.webp',
       'text': 'Exceptional eyesight and tracking abilities, can see far distances and follow scents with ease. Starlight once located a lost child who had wandered away from their campsite, using their keen sense of smell to follow the child\'s scent.'
     }
   ]
