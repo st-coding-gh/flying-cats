a54a76e53aba67f7989418d11b78829387b727cf
st-coding-gh <st-coding@yandex.ru>
2023-03-03 21:17:47 +0300

    fix errors
    1. burger is now transforms to cross and backwards 2. burger is now reduced in size 3. when burger menu is opened - scrolling is disabled (main is hidden)

--- src/scripts/js/burger-action.js
+++ src/scripts/js/burger-action.js
@@ -4,10 +4,16 @@ export function burgerAction () {
 const burger = document.querySelector('.burger');
 const nav = document.querySelector('.nav--toggle');
 const navLink = document.querySelectorAll('.nav-item');
+const burgerLines = document.querySelector('.burger-lines');
+const burgerCross = document.querySelector('.burger-cross');
+const main = document.querySelector('.main');
 
 // functions declaration
   burger.addEventListener('click', (event) => {
     nav.classList.toggle('hidden--toggle');
+    burgerLines.classList.toggle('hidden--toggle');
+    burgerCross.classList.toggle('hidden--toggle');
+    main.classList.toggle('hidden--toggle');
   });
   for (let each of navLink) {
     each.addEventListener('click', (event) => {
