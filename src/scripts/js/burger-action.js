f6b1d4af85edf0d2341c3569da3e4beb97b3807b
st-coding-gh <st-coding@yandex.ru>
2023-03-03 22:08:45 +0300

    test

--- src/scripts/js/burger-action.js
+++ src/scripts/js/burger-action.js
@@ -4,23 +4,28 @@ export function burgerAction () {
 const burger = document.querySelector('.burger');
 const nav = document.querySelector('.nav--toggle');
 const navLink = document.querySelectorAll('.nav-item');
-const burgerLines = document.querySelector('.burger-lines');
-const burgerCross = document.querySelector('.burger-cross');
 const main = document.querySelector('.main');
+const lines = document.querySelectorAll('.burger-line');
+console.log(lines);
+
 
 // functions declaration
   burger.addEventListener('click', (event) => {
     nav.classList.toggle('hidden--toggle');
-    burgerLines.classList.toggle('hidden--toggle');
-    burgerCross.classList.toggle('hidden--toggle');
     main.classList.toggle('hidden--toggle');
+    for (let each of lines) {
+      each.classList.toggle('burger-crossLine');
+    }
+    lines[1].classList.toggle('hidden--toggle');
   });
   for (let each of navLink) {
     each.addEventListener('click', (event) => {
       nav.classList.toggle('hidden--toggle');
-      burgerLines.classList.toggle('hidden--toggle');
-      burgerCross.classList.toggle('hidden--toggle');
       main.classList.toggle('hidden--toggle');
+      for (let each of lines) {
+        each.classList.toggle('burger-crossLine');
+      }
+      lines[1].classList.toggle('hidden--toggle');
     });
   }
 }
