0ddeb4868119deed56180030c75e3bf630d3ceda
st-coding-gh <st-coding@yandex.ru>
2023-03-05 08:41:47 +0300

    fix as per brother's comments
    1) simplified burgerAction 2) removed multiple toggle selectors - only nav-open is now being toggled 3) simplified burger spans

--- src/scripts/js/burger-action.js
+++ src/scripts/js/burger-action.js
@@ -1,31 +1,10 @@
 export function burgerAction () {
-// CONST and VAR declaration
+  const toggleLink = document.querySelectorAll('.toggle-link');
 
-const burger = document.querySelector('.burger');
-const nav = document.querySelector('.nav--toggle');
-const navLink = document.querySelectorAll('.nav-item');
-const main = document.querySelector('.main');
-const lines = document.querySelectorAll('.burger-line');
-console.log(lines);
-
-
-// functions declaration
-  burger.addEventListener('click', (event) => {
-    nav.classList.toggle('hidden--toggle');
-    main.classList.toggle('hidden--toggle');
-    for (let each of lines) {
-      each.classList.toggle('burger-crossLine');
-    }
-    lines[1].classList.toggle('hidden--toggle');
+  toggleLink.forEach((link)=>{
+    link.addEventListener('click', () => {
+      const body = document.querySelector('body');
+      body.classList.toggle('nav-open');
+    })
   });
-  for (let each of navLink) {
-    each.addEventListener('click', (event) => {
-      nav.classList.toggle('hidden--toggle');
-      main.classList.toggle('hidden--toggle');
-      for (let each of lines) {
-        each.classList.toggle('burger-crossLine');
-      }
-      lines[1].classList.toggle('hidden--toggle');
-    });
-  }
 }
